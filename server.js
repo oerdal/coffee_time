617dea0b277856ea7ba63435d46d331d28ac77c3
Aashray <aashray@uw.edu>
2018-11-25 21:56:15 -0800

    updated server.js to include endpoint for fetching drink data for a specific customer or store, from an ID in the querystring

--- server.js
+++ server.js
@@ -125,6 +125,43 @@ app.post("/login", function(req, res) {
     })
 })
 
+
+/* 
+ * route to get current drinks for a store or a user
+ * if a user id is passed in the querystring, will query
+ * the database for drinks matching the customer id, otherwise
+ * if a store id is passed, will query the database for drinks matching
+ * the store ID, otherwise will return nothing if neither are passed
+*/
+app.get('/user', (req, res) => {
+    var id = ''
+    var query = ''
+    if(req.query.userId){
+        id = req.query.userId
+        console.log("user ID is... " + id)
+        query = "SELECT * FROM user_drinks JOIN users on users_drinks.user_id = " +
+                "users.id JOIN drinks on users_drinks.id = drinks.id WHERE users.id = {0}".format(id)
+        
+    } else if(req.query.storeId){
+        id = req.query.storeId
+        console.log("store ID is... " + id)
+        query = "SELECT * FROM user_drinks JOIN users on user_drinks.store_id = " +
+                "users.store_id JOIN drinks on user_drinks.id = drinks.id WHERE users.store_id = {0}".format(id)
+    } else {
+        return res.send(JSON.stringify({"status": 404, "error": "no user or store ID found"}))
+    }
+    con.query(query, (error, result) => {
+        if(error){
+            return res.send(JSON.stringify({"status": 404, "response": null, "id": id}))
+        }
+        // if we can successfully query the drink table for the drinks for a given
+        // user or store, return the appropriate response
+        return res.send(JSON.stringify({"status" : 200, "response": result, "id": id}))
+    })
+})
+
+// route to get current drinks for a given store
+
 // Listen on port 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
