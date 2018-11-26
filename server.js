9ec76682f516cffb5e50f8069bc73c73a3963807
Andre Huang <huangand000@gmail.com>
2018-11-25 17:11:49 -0800

    Coffee Project: Added barebones customerRegister and customerLogin endpoints WITHOUT OAuth to back-end API

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
     var query = "SELECT * from users"
     con.query(query, function (error, result) {
         if (error) {
-            throw error;
+            return res.json({status: "Error", data: error});
         } else {
             console.log(result);
         }
@@ -51,6 +51,58 @@ app.get('/', function(req, res) {
 
     // Render a temporarily front-end using ejs in views file
     res.render("index");
+})
+
+app.post("/customerRegister", function(req, res) {
+    if (req.body.password != req.body.passwordConfirm) {
+        return res.json({status: "Error", data: "Password does not match"});
+    } 
+    var query = "SELECT users.username FROM users WHERE users.username = {0};".format(req.body.username);
+    con.query(query, function(error, user) {
+        if (error) {
+            return res.json({status: "Error", data: error});
+        } else {
+            if (user.length != 0) {
+                return res.json({status: "Error", data: "Username already exists in the database" });
+            } else {
+                var successQuery = "INSERT INTO coffeeDB.users (name, username, password, created_at, updated_at)VALUES ({0}, {1}, {2}, {3}, {4});".format(req.body.name, req.body.username, req.body.password, NOW(), NOW());
+                con.query(query, function (error, result) {
+                    if (error) {
+                        return res.json({status: "Error", data: error});
+                    } else {
+                        return res.json({status: "Success", data: result});
+                    }
+                })
+            }
+        }
+    })
+})
+
+app.post("/customerLogin", function(req, res) {
+    var emailQuery = "SELECT users.username AS username FROM users WHERE users.username={0}".format(req.body.username);
+    con.query(emailQuery, function(error, result) {
+        if (error) {
+            return res.json({status: "Error", data: error});
+        } else {
+            if (result.length == 0) {
+                return res.json({status: "Error", data: "Username does not exist in the database" });
+            } else {
+                var passwordQuery = "SELECT * FROM users WHERE users.username={0} AND users.password={1}".format(req.body.username, req.body.password);
+                con.query(passwordQuery, function(error, result) {
+                    if (error) {
+                        return res.json({status: "Error", data: error});
+                    } else {
+                        if (result.length == 0) {
+                            return res.json({status: "Error", data: "Username and Password does not match"});
+                        } else {
+                            return res.json({status: "Success", data: result})
+                        }
+                    }
+                })
+                return res.json({status: "Success", data: result});
+            }
+        }
+    })
 })
 
 // Listen on port 8000
