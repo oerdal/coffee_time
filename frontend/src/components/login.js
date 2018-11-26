4aea084c27e23e351aa4950fd0dd8a75754de413
Ozan Erdal <oerdal@Ozans-MacBook-Pro.local>
2018-11-25 16:13:46 -0800

    Coffee Project: Added submit functionality

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Login = () => {
     return (
         <div className="container w-50" id="login">
-            <div className="container d-flex flex-column p-3 border rounded border-dark" id="existing-login">
-                <form action="/login" method="POST">
+            <div className="container p-3 border rounded border-dark align-items-center" id="existing-login">
+                <form action="/login" method="POST" className="d-flex flex-column">
                     <div className="form-group w-75 mx-auto p-2">
                         <label for="userField">Username</label>
                         <input type="text" name="username" id="userField" className="form-control" placeholder="Enter Username" aria-describedby="userHelp" />
@@ -13,8 +13,8 @@ const Login = () => {
                         <label for="passField">Password</label>
                         <input type="text" name="password" id="passField" className="form-control" placeholder="Enter Password" aria-describedby="passHelp" />
                     </div>
+                    <button className="btn btn-link btn-outline-dark p-2 mx-auto" type="submit">Login</button>
                 </form>
-                <button className="btn btn-link btn-outline-dark p-2 mx-auto">Login</button>
             </div>
             <div className="container d-flex justify-content-around my-3 p-3" id="other-login">
                 <button className="btn btn-link btn-outline-dark p-2">Create Account</button>
